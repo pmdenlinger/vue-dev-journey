557f03a1649796c7845bcfff0a6c8b2a0aa52132
Paul Denlinger <pmdenlinger@icloud.com>
2026-03-13 14:42:43 -0700

    Create practice directory and new exercise1_01, run vite and fill out basic code

---
+++ vuejs3-projects-book/01/practice/exercise1_01/src/App.vue
@@ -0,0 +1,13 @@
+/** src/App.vue **/
+<template>
+  <Exercise />
+</template>
+<script>
+import Exercise from './components/Exercise1-01.vue';
+export default {
+  components: {
+    Exercise,
+  },
+};
+</script>
+<style></style>
