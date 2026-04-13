42a42b2a6962616fbbc8ec6a586eb8487d96a122
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-13 14:39:03 -0700

    Add increment() and v-on:click attribute to index page

--- vuejs3-mastery-ztm/section-2-1-STARTER/app.js
+++ vuejs3-mastery-ztm/section-2-1-STARTER/app.js
@@ -3,12 +3,16 @@ const vm = Vue.createApp({
     return {
       firstName: 'John',
       lastName: 'Doe',
-      url: "https://www.google.com"
+      raw_url: "https://www.google.com",
+      age: 20
     };
   },
   methods: {
     fullName() {
         return `${ this.firstName } ${this.lastName.toUpperCase()}`
+    },
+    increment(){
+        this.age++
     }
   }
 }).mount('#app');
