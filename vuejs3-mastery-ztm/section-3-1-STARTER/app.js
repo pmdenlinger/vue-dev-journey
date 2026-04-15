68c76a25051cbb4ae14356db0cd32cb6c9c59ac7
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-15 10:56:43 -0700

    Corrected issue in CSS transform syntax

--- vuejs3-mastery-ztm/section-3-1-STARTER/app.js
+++ vuejs3-mastery-ztm/section-3-1-STARTER/app.js
@@ -1,31 +1,30 @@
 Vue.createApp({
-    data() {
-        return {
-            perspective: 0,
-            rotateX: 0,
-            rotateY: 0, 
-            rotateZ: 0
-        }
-    },
-    computed: {
-        box(){
-            return {
-                transform: 
+  data() {
+    return {
+      perspective: 0,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
+    };
+  },
+  computed: {
+    box() {
+      return {
+        transform: `
+                perspective(${this.perspective}px)
+                rotateX(${this.rotateX}deg)
+                rotateY(${this.rotateY}deg)
+                rotateZ(${this.rotateZ}deg)
                 `
-                perspective(this.perspective(${this.perspective}px)),
-                rotateX: rotateX(${this.rotateX}deg),
-                rotateY: rotateY(${this.rotateY}deg),
-                rotateZ: rotateZ(${this.rotateZ}deg)
-                `
-            }
-        }
+      };
     },
-    methods: {
-        reset() {
-            this.perspective = 100
-            this.rotateX = 0
-            this.rotateY = 0
-            this.rotateZ = 0
-        }
-    }
-}).mount("#app")
+  },
+  methods: {
+    reset() {
+      this.perspective = 100;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+    },
+  },
+}).mount('#app');
