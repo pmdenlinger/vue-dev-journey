c44c1cf8d9addf6636f3dbdc1f2b6bcac1545dd3
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-15 10:22:30 -0700

    Add Vue.createApp method and ount 'App' then add data object and add four properties with initial values

---
+++ vuejs3-mastery-ztm/section-3-1-STARTER/app.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+    data() {
+        return {
+            perspective: 0,
+            rotateX: 0,
+            rotateY: 0, 
+            rotateZ: 0
+        }
+    }
+}).mount("#app")
