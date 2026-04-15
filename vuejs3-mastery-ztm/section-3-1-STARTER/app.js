8773151d386748bfc32f5e7f8fb7036f84dbc3fd
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-15 10:45:35 -0700

    Add reset method

--- vuejs3-mastery-ztm/section-3-1-STARTER/app.js
+++ vuejs3-mastery-ztm/section-3-1-STARTER/app.js
@@ -19,5 +19,13 @@ Vue.createApp({
                 `
             }
         }
+    },
+    methods: {
+        reset() {
+            this.perspective = 100
+            this.rotateX = 0
+            this.rotateY = 0
+            this.rotateZ = 0
+        }
     }
 }).mount("#app")
